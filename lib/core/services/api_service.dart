b570c27396f6f3e1ebc95f8ca9228c0987c2d3bf
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-19 02:53:32 +0100

    add failure class + api service

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,12 @@
+import 'package:caching/core/constants/api_constants.dart';
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio;
+  ApiService(this.dio);
+
+  Future<Map<String, dynamic>> get() async {
+    var response = await dio.get(EnvironmentVariables.getProduct);
+    return response.data;
+  }
+}
---
+++ lib/feature/home/data/repos/product_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:caching/feature/home/domain/entities/product_entity.dart';
+import 'package:caching/feature/home/domain/repos/product_repostry.dart';
+
+class ProductRepoImpl implements ProductRepostry {
+  @override
+  Future<ProductEntity> getAllProducts() {
+    // TODO: implement getAllProducts
+    throw UnimplementedError();
+  }
+}
