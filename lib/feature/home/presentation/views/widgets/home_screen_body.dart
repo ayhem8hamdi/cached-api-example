25c60699e4857423f66f9d52fc3e1b9905cfebca
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-08-31 01:47:19 +0100

    create product item part 1

--- lib/feature/home/presentation/views/widgets/home_screen_body.dart
+++ lib/feature/home/presentation/views/widgets/home_screen_body.dart
@@ -1,5 +1,6 @@
 import 'package:caching/feature/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
@@ -7,8 +8,64 @@ class HomeScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: CustomScrollView(
-        slivers: [CustomHomeAppBar()],
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: CustomScrollView(
+          slivers: [CustomHomeAppBar(), SliverGap(25), ProductItem()],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+              flex: 2,
+              child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(8)),
+                  ))),
+          Expanded(
+              flex: 3,
+              child: Padding(
+                padding: EdgeInsets.only(left: 5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Product 1'),
+                    Gap(4),
+                    Text('others few us role set '),
+                    Gap(4),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text('Created At'), Text('24-04-2004')],
+                    ),
+                    Gap(5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text('Updated At'), Text('24-04-2004')],
+                    ),
+                    Gap(5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [Text('Created At'), Text(r'988$')],
+                    )
+                  ],
+                ),
+              ))
+        ],
       ),
     );
   }
